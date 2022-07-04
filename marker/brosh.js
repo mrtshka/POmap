cdb6882836940ad7587a2b0e3eb4e94648587b45
mrtshka <108582988+mrtshka@users.noreply.github.com>
2022-07-04 12:34:32 +0000

    Add files via upload

---
+++ marker/brosh.js
@@ -0,0 +1,77 @@
+var brosh = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "",
+                "image_id": "msk-glav"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    171.1875,
+                    -7.828125
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "",
+                "image_id": "gaz3110"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    164.484375,
+                    -6.109375
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "",
+                "image_id": "2101obnov"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    124.125,
+                    -149
+
+               ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "",
+                "image_id": "agpmsk"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    24.375,
+                    -173.40625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "",
+                "image_id": "mrk"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    105.3125,
+                    -101.15625
+
+                ]
+            }
+        }
+    ]
+};
